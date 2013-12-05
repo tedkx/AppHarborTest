4259a623284982161247e538c6b767863f053268
TedKX <thodwriskalaitzidis@hotmail.com>
2013-12-05 16:20:58 +0200

    appfail

--- AppHarborTest/Models/DefaultContext.cs
+++ AppHarborTest/Models/DefaultContext.cs
@@ -14,8 +14,7 @@ namespace AppHarborTest.Models
         // 
         // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<AppHarborTest.Models.DefaultContext>());
 
-        public DefaultContext()
-            : base(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultContextHarbor"].ConnectionString)
+        public DefaultContext() : base(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultContextHarbor"].ConnectionString)
         {
             if (Steps.Count() == 0) { InitData(); }
         }
