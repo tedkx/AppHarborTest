f8bf64998070217c2e69a6f615da0ec13faaac25
TedKX <thodwriskalaitzidis@hotmail.com>
2013-12-05 13:04:11 +0200

    connstr

--- AppHarborTest/Models/DefaultContext.cs
+++ AppHarborTest/Models/DefaultContext.cs
@@ -14,7 +14,7 @@ namespace AppHarborTest.Models
         // 
         // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<AppHarborTest.Models.DefaultContext>());
 
-        public DefaultContext() : base("name=DefaultContext")
+        public DefaultContext() : base(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultContext"].ConnectionString)
         {
             if (Steps.Count() == 0) { InitData(); }
         }
